0a7c588c8e010b7d8036600107cac4f808d244c9
clakr <clarktolosa@gmail.com>
2023-05-14 18:54:35 +0800

    to deploy

--- src/main.ts
+++ src/main.ts
@@ -14,4 +14,32 @@ function changeTextContent(element: HTMLTitleElement | Element | null) {
   changeTextContent(heading);
 
   document.documentElement.setAttribute("data-theme", "dark");
+
+  const lists = document.querySelectorAll("li");
+  lists.forEach((list) =>
+    list.addEventListener("click", function () {
+      const activeList = document.querySelector("li.active");
+
+      activeList?.classList.remove("active");
+      this.classList.add("active");
+    })
+  );
+
+  const btnSubmit = document.querySelector(".card__cta");
+  btnSubmit?.addEventListener("click", function () {
+    const activeList = document.querySelector("li.active");
+    if (!activeList) return;
+
+    const [form, thanks] = document.querySelectorAll<HTMLDivElement>(
+      'div[class^="card--"]'
+    );
+
+    form.style.display = "none";
+    thanks.style.display = "flex";
+
+    const selectedList = document.getElementById("selected");
+    if (!selectedList) return;
+
+    selectedList.textContent = activeList.textContent;
+  });
 })();
